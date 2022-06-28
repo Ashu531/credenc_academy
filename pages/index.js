7e52282e941ca5299261fe3016e4cdb4ac68db8a
Apple <apple@LT30246.local>
2022-06-28 12:03:46 +0530

    browser preference set

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
 import Dashboard from './dashboard'
-import React from "react"
+import React,{useState,useEffect} from "react"
 import useLocalStorage from "use-local-storage";
 const theme = 'credenc-edtech-theme';
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
   const [theme,setTheme]= useLocalStorage("theme" ? "dark" : "light");
 
+  useEffect(() => {
+    setMounted(true);
+}, []);
+
   const toggleTheme=()=>{
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      // dark mode
+      setTheme("dark")
+  }else{
     let newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme)
   }
+  
+   
+  }
 
   return (
-    <div data-theme={theme}>
-     <Dashboard toggleTheme={toggleTheme} />
-    </div>
+    <>
+    {
+    mounted &&  <div data-theme={theme}>
+    <Dashboard toggleTheme={toggleTheme} />
+   </div>
+  }
+   </>
   )
 }
 
